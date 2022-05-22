217e86f7c0739a322eef8cb469c2a77a71ebfd6e
wuyafeiya <wuyafeiya@gmail.com>
2022-05-22 16:04:42 +0800

    布局

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,30 +7,29 @@
       title="提示"
       :visible.sync="centerDialogVisible"
       width="80%"
-      center
+      centers
+      custom-class="Tree"
     >
       <el-row type="flex" justify="space-between">
-        <el-col :span="4">
-          <el-tree
-            :data="data"
-            :props="defaultProps"
-            @node-click="handleNodeClick"
-          ></el-tree>
+        <el-col :span="4" style="border: 1px solid red">
+          <el-scrollbar style="height: 100%">
+            <el-tree
+              :data="data"
+              :props="defaultProps"
+              @node-click="handleNodeClick"
+            ></el-tree>
+          </el-scrollbar>
         </el-col>
         <el-col :span="2" style="text-align: center">
           <div>
-            <el-button type="danger">添加字段</el-button>
-            <el-button type="danger">添加字段</el-button>
-            <el-button type="danger">添加字段</el-button>
-            <el-button type="danger">添加字段</el-button>
-            <el-button type="danger">添加字段</el-button>
+            <el-button type="danger" v-for="item in AddList" :key="item.id">{{
+              item.name
+            }}</el-button>
           </div>
           <div style="margin: 20px 0">
-            <el-button type="primary">添加字段</el-button>
-            <el-button type="primary">添加字段</el-button>
-            <el-button type="primary">添加字段</el-button>
-            <el-button type="primary">添加字段</el-button>
-            <el-button type="primary">添加字段</el-button>
+            <el-button type="primary" v-for="item in MoveList" :key="item.id">{{
+              item.name
+            }}</el-button>
           </div>
           <div>
             <el-button type="primary" style="width: 98px">保存</el-button>
@@ -56,49 +55,177 @@ export default {
   data() {
     return {
       centerDialogVisible: false,
-      tableData: [
+      AddList: [
+        { id: 'Addfiled', name: '增加字段' },
+        { id: 'Delfiled', name: '删除字段' },
+        { id: 'Addor', name: '添加"or"' },
+        { id: 'Addand', name: '添加"and"' },
+        { id: 'Addxor', name: '添加"xor"' }
+      ],
+      MoveList: [
+        { id: 'TopMove', name: '上移' },
+        { id: 'DownMove', name: '下移' },
+        { id: 'MoveIn', name: '移进' },
+        { id: 'MoveOut', name: '移出' },
+        { id: 'Test', name: '检验' }
+      ],
+      data: [
         {
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
+          label: '一级 1',
+          children: [
+            {
+              label: '二级 1-1',
+              children: [
+                {
+                  label: '三级 1-1-1'
+                }
+              ]
+            }
+          ]
         },
         {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
+          label: '一级 2',
+          children: [
+            {
+              label: '二级 2-1',
+              children: [
+                {
+                  label: '三级 2-1-1'
+                }
+              ]
+            },
+            {
+              label: '二级 2-2',
+              children: [
+                {
+                  label: '三级 2-2-1'
+                }
+              ]
+            }
+          ]
         },
         {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
+          label: '一级 3',
+          children: [
+            {
+              label: '二级 3-1',
+              children: [
+                {
+                  label: '三级 3-1-1'
+                }
+              ]
+            },
+            {
+              label: '二级 3-2',
+              children: [
+                {
+                  label: '三级 3-2-1'
+                }
+              ]
+            }
+          ]
         },
         {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
+          label: '一级 4',
+          children: [
+            {
+              label: '二级 4-1',
+              children: [
+                {
+                  label: '三级 4-1-1'
+                }
+              ]
+            },
+            {
+              label: '二级 4-2',
+              children: [
+                {
+                  label: '三级 4-2-1'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          label: '一级 5',
+          children: [
+            {
+              label: '二级 5-1',
+              children: [
+                {
+                  label: '三级 5-1-1'
+                }
+              ]
+            },
+            {
+              label: '二级 5-2',
+              children: [
+                {
+                  label: '三级 5-2-1'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          label: '一级 6',
+          children: [
+            {
+              label: '二级 6-1',
+              children: [
+                {
+                  label: '三级 6-1-1'
+                }
+              ]
+            },
+            {
+              label: '二级 6-2',
+              children: [
+                {
+                  label: '三级 6-2-1'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          label: '一级 7',
+          children: [
+            {
+              label: '二级 7-1',
+              children: [
+                {
+                  label: '三级 7-1-1'
+                }
+              ]
+            },
+            {
+              label: '二级 7-2',
+              children: [
+                {
+                  label: '三级 7-2-1'
+                }
+              ]
+            }
+          ]
         }
       ]
-    }
-  },
-  methods: {
-    count() {
-      console.log(123)
     }
   }
 }
 </script>
 <style scoped>
-/* .el-row {
-  margin-bottom: 20px;
-  &:last-child {
-    margin-bottom: 0;
-  }
-} */
+::v-deep .tree {
+  background-color: aqua;
+}
 .el-col {
   border-radius: 4px;
 }
 .bg-purple-dark {
   background: #99a9bf;
+}
+.el-col-4 {
+  height: 600px;
 }
 .bg-purple {
   background: #d3dce6;
@@ -116,11 +243,19 @@ export default {
 }
 
 ::v-deep .el-button {
+  width: 98px;
   margin-left: 0;
   margin-bottom: 6px;
 }
 
 ::v-deep .el-table {
   margin: auto;
+}
+::v-deep .el-scrollbar__wrap {
+  overflow-x: hidden;
+  /* top: -90px; */
+}
+.el-dialog__wrapper {
+  top: -90px;
 }
 </style>
